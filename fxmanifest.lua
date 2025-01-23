bba2283f547f9b7e2163200cf41ea6728140f577
Maximus7474 <the.maximus.7474@gmail.com>
2025-01-23 16:12:58 +0100

    feat: fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+files {
+    'data/**/*.meta',
+	'audio/*game.dat151.rel',
+	'audio/*sounds.dat54.rel',
+	'audio/sfx/**/*.awc'
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'DLCTEXT_FILE' 'data/dlctext.meta'
+data_file 'CARCONTENTUNLOCKS_FILE' 'data/contentunlocks.meta'
+data_file 'WEAPON_METADATA_FILE' 'data/vehicleweapon*.meta'
+data_file 'WEAPONINFO_FILE' 'data/*vehicleweapon*.meta'
+
+data_file 'AUDIO_GAMEDATA' 'audio/dlc24-2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/dlc24-2_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_24-2'
